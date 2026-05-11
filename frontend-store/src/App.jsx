9f1c5987e62fae5751c4283adcf733168487905b
lengyelluk <lengyel.luk@gmail.com>
2026-05-11 11:13:41 +0200

    chore: trigger pipeline

--- frontend-store/src/App.jsx
+++ frontend-store/src/App.jsx
@@ -21,7 +21,7 @@ const STEPS = [
 
 const INITIAL_DATA = {
   // About You (height/gender are frontend-only - 3 not sent to backend)
-  //TODO: send them to backend so they might be eventually used based on nutrionist
+  //TODO: send them to backend so they might be eventually used based on nutrionists
   gender: '',
   age: '',
   height_cm: '',
